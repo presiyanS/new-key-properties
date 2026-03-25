f11b698d2d6511b814b1d2ef5edc199b10560243
Presiyan Sokolov <presiyansokolov@192.168.100.28>
2026-03-25 13:23:37 +0200

    Fix Vercel deployment issues

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -3,8 +3,8 @@ import imageUrlBuilder from '@sanity/image-url'
 import type { SanityImageSource } from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ?? '9gz26s06',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET ?? 'production',
   apiVersion: '2024-01-01',
   useCdn: true,
 })
